7b7dc977a45c69156c5baee94ed3ced6521b155d
jayak <kumarijaya127@gmail.com>
2024-02-24 16:00:02 +0530

    added sample text

--- studentFrontend/src/Edit.jsx
+++ studentFrontend/src/Edit.jsx
@@ -44,6 +44,7 @@ function Edit() {
     <div>
       <div className="d-flex vh-100 bg-primary justify-content-center align-items-center ">
         <div className="w-50 bg-white rounded p-3">
+          <h1>student update form</h1>
           <form onSubmit={handleUpdate}>
             <h2> Update Student</h2>
             <div className="mb-2">
